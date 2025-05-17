ed5696e04f0933911e45b59202a06ec57657393c
petergpt <130558610+petergpt@users.noreply.github.com>
2025-05-17 23:47:06 +0100

    feat: add initial 3d scene

---
+++ token-trek/src/components/GameScene.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Player from './Player'
+
+const GameScene: FC = () => {
+  return (
+    <Canvas
+      camera={{ position: [0, 2, 5], fov: 60 }}
+      style={{ height: '100vh' }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <Player position={[0, 0.5, 0]} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
+
+export default GameScene
